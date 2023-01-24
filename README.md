a6dd486894fcbef1c0512bab7452cdf6cd40baff
CarinaLogic <122521908+carinalogic@users.noreply.github.com>
2023-01-24 10:56:23 +0900

    Imported libGDX, hellfire begins

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# rpg
+# RPG
+An open-source side-scroller action rpg. Made using libGDX.
+## Note
+This is a personal project made by a CS student with little java experiences. 
+Do not expect me to make a high-quality code.
+Also, I will never make it pay-to-win / put adverts for profit.
+My goal is to make an enjoyable game while getting touch with java, and nothing more.
---
+++ core/src/me/carina/rpg/GameInstance.java
@@ -0,0 +1,33 @@
+package me.carina.rpg;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GameInstance extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+		Array<String> array = new Array<>();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
