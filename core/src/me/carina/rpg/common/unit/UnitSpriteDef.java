6d35f9c35aef922424a20c8e27bc2dc1e82e3180
CarinaLogic <122521908+carinalogic@users.noreply.github.com>
2023-02-27 20:41:30 +0900

    Found a better way to do json

--- core/src/me/carina/rpg/common/unit/UnitSpriteDef.java
+++ core/src/me/carina/rpg/common/unit/UnitSpriteDef.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.utils.ObjectMap;
 import java.util.Map;
 
 public class UnitSpriteDef {
+    String name;
     Array<UnitSpriteEntryDef> entries;
     public UnitSpriteDef(){} // for json loading
+
+
 }
--- core/src/me/carina/rpg/common/unit/UnitSpriteEntryDef.java
+++ core/src/me/carina/rpg/common/unit/UnitSpriteEntryDef.java
@@ -3,37 +3,14 @@ package me.carina.rpg.common.unit;
 import com.badlogic.gdx.utils.Array;
 
 public class UnitSpriteEntryDef {
-    //One definition per one (potentially shared) palette
-    String name;
-    PaletteDef palette;
-    Array<UnitSpriteTypeEntryDef> sprites;
+    UnitSpriteType type;
+    String palette;
+    int size;
+    Array<Connection> connections;
     public UnitSpriteEntryDef(){} //for json loading
-    public static class UnitSpriteTypeEntryDef{
+    public static class Connection{
         UnitSpriteType type;
-        int size;
-        int width;
-        int height;
-        int originX;
-        int originY;
-        Array<IndividualSpriteData> data;
-        public UnitSpriteTypeEntryDef(){} //for json loading
-    }
-    public static class IndividualSpriteData {
-        Array<ConnectionInfo> connections;
-        int rotation; //in degrees
-    }
-    public static class PaletteDef{
-        int colors;
-        int entrySize;
-        int originX;
-        int originY;
-        public PaletteDef(){}//for json loading
-    }
-    public static class ConnectionInfo{
-        UnitSpriteType type;
-        int x; //Absolute coords, count from very top left of the png
-        int y; //You probably want coords between pixels, just pick the coords
-               // of the pixel whose top left corner is touching the desired coords
-        public ConnectionInfo(){}//for json loading
+        int x;
+        int y;
     }
 }
