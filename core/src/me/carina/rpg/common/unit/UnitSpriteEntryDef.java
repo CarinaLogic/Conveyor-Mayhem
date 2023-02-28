42feac6e1a422453e8704aa5d0c4737d975b8cb5
CarinaLogic <122521908+carinalogic@users.noreply.github.com>
2023-02-28 17:58:02 +0900

    Json loading, 3rd iteration

--- core/src/me/carina/rpg/common/unit/UnitSpriteEntryDef.java
+++ core/src/me/carina/rpg/common/unit/UnitSpriteEntryDef.java
@@ -6,8 +6,18 @@ public class UnitSpriteEntryDef {
     UnitSpriteType type;
     String palette;
     int size;
-    Array<Connection> connections;
+    int defaultIndex;
+    Array<Connection> centralConnections;
+    Array<Connections> additionalConnections;
     public UnitSpriteEntryDef(){} //for json loading
+    public static class Coords {
+        int x;
+        int y;
+    }
+    public static class Connections{
+        UnitSpriteType type;
+        Array<Coords> coords;
+    }
     public static class Connection{
         UnitSpriteType type;
         int x;
