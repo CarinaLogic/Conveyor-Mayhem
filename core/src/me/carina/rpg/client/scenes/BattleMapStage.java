9033ffa09ca3bf67f6d2d343720cecd97c6deb09
CarinaLogic <122521908+carinalogic@users.noreply.github.com>
2023-03-19 21:15:13 +0900

    Updated BattleMapActor

--- core/src/me/carina/rpg/client/scenes/BattleMapStage.java
+++ core/src/me/carina/rpg/client/scenes/BattleMapStage.java
@@ -5,17 +5,23 @@ import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
+import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.*;
+import com.kotcrab.vis.ui.VisUI;
+import com.kotcrab.vis.ui.layout.DragPane;
+import com.kotcrab.vis.ui.widget.Draggable;
 import me.carina.rpg.common.map.*;
 
 public class BattleMapStage extends GameStage {
-    Table table;
-    TileActor[] tileActor = new TileActor[10];
+    BattleMapActor battleMapActor;
+
     public BattleMapStage(){
         super(new PerspectiveViewport(10,10,new PerspectiveCamera(70,10,10)));
     }
@@ -26,7 +32,9 @@ public class BattleMapStage extends GameStage {
         getCamera().far = 100;
         getCamera().position.set(5,3,8);
         getCamera().lookAt(5,5,0);
-        addActor(new BattleMapActor(new BattleMap(game)));
-        setDebugUnderMouse(true);
+        battleMapActor = new BattleMapActor(new BattleMap(game));
+        battleMapActor.setTouchable(Touchable.enabled);
+        addActor(battleMapActor);
+        setDebugAll(true);
     }
 }
--- core/src/me/carina/rpg/common/map/BattleMapActor.java
+++ core/src/me/carina/rpg/common/map/BattleMapActor.java
@@ -1,6 +1,8 @@
 package me.carina.rpg.common.map;
 
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.kotcrab.vis.ui.widget.Draggable;
+import me.carina.rpg.client.misc.Movable;
 
 public class BattleMapActor extends Group{
     BattleMap map;
@@ -11,5 +13,7 @@ public class BattleMapActor extends Group{
                 addActor(tile.newActor());
             }
         }
+        setSize(map.getWidth(), getHeight());
+        addListener(new Movable());
     }
 }
