5158c80d5ff2a5de7fe545ce69562282223c94e8
CarinaLogic <carina.logic@gmail.com>
2023-08-17 21:03:50 +0900

    Somewhat working scrollpane

--- core/src/me/carina/rpg/client/ui/UIVerticalListView.java
+++ core/src/me/carina/rpg/client/ui/UIVerticalListView.java
@@ -1,18 +1,35 @@
 package me.carina.rpg.client.ui;
 
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+import me.carina.rpg.Game;
+import me.carina.rpg.common.file.AssetGroup;
+import me.carina.rpg.common.file.Path;
 
-public class UIVerticalListView extends UITableView{
+public class UIVerticalListView extends ScrollPane{
+    Table innerTable;
     public UIVerticalListView(){
-
+        super(null,getPaneStyle());
+        innerTable = new Table();
+        setActor(innerTable);
+        innerTable.pad(6);
     }
 
-    @Override
     public <T extends Actor> Cell<T> add(T actor) {
-        Cell<T> cell = super.add(actor);
+        Cell<T> cell = innerTable.add(actor);
         cell.expandX().fillX();
-        row();
+        innerTable.row();
         return cell;
+    }
+
+    public static ScrollPane.ScrollPaneStyle getPaneStyle(){
+        ScrollPane.ScrollPaneStyle style = new ScrollPane.ScrollPaneStyle();
+        style.background =Game.getClient().getAssets().get(new Path("core", AssetGroup.ui, "frame"), NinePatchDrawable.class);
+         return style;
     }
 }
